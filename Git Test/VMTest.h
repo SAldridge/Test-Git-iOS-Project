37bfbc8b115451667ff88fcce184f998ae35f01c
SAldridge <VarMedia@VM-Mac.local>
2014-07-18 12:21:14 -0400

    first commit

---
+++ Git Test/VMTest.h
@@ -0,0 +1,13 @@
+//
+//  VMTest.h
+//  Git Test
+//
+//  Created by VarMedia on 7/16/14.
+//  Copyright (c) 2014 VarMobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VMTest : NSObject
+
+@end
---
+++ Git Test/VMTest.m
@@ -0,0 +1,13 @@
+//
+//  VMTest.m
+//  Git Test
+//
+//  Created by VarMedia on 7/16/14.
+//  Copyright (c) 2014 VarMobile. All rights reserved.
+//
+
+#import "VMTest.h"
+
+@implementation VMTest
+
+@end
